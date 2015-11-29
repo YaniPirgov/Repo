9376cecb07e69a26431974bd6a3b9cb02d01abd2
Yani Pirgov <jani_boy@abv.bg>
2015-11-29 23:15:30 +0200

    First week task(contact form, send to database, recaptcha) - complete (except gmail configuration in my account)

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -1,36 +1,25 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
 	public function index()
 	{
-		$this->load->helper(array('form', 'html', 'url'));
-		$this->load->library('form_validation');
-		$this->load->view('index');
-	}
-
-	public function contact()
-	{
-		$this->load->library("form_validation");
-		$this->form_validation->set_rules('name', 'Name', 'required|alpha');
-		$this->form_validation->set_rules('email', 'Email', 'required');
-		$this->form_validation->set_rules('message', 'Messsage', 'required');
-
-		if($this->form_validation->run() === false)
-		{
-			echo 'unsuccessfull';
-		}
-		else
-		{
-			$post_name = $this->input->post('name');
-			$post_email = $this->input->post('email');
-			$post_message = $this->input->post('message');
-			echo 'successfull!';
-			echo 'test';
-			$this->contact_model->save_contact($post_name, $post_email, $post_message);
-		}
+		$this->load->view('welcome_message');
 	}
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
